Phonebooth/esaml

commit c7177f8d520d2cb2c9031fec544449cd6cd8cee6

Refactoring: esaml_util and esaml_sp
 * esaml_sp is now more independent of cowboy, so it could
   be used without it
 * esaml_util is a new module containing some utility funcs
   that used to be in esaml:, plus all the ETS table and
   cert/key loading stuff

src/esaml_sp_default.erl
@@ -11,16 +11,22 @@
 
 -include("esaml.hrl").
 
--export([init/2, handle_assertion/3, terminate/2]).
+-export([init/2, check_duplicate/3, handle_consume/2, terminate/1]).
 
-init(Req, _Args) -> {ok, Req, none}.
+init(Req, _Args) -> {ok, Req}.
 
-handle_assertion(Req, Assertion, State) ->
+check_duplicate(Assertion, Sha1, Req) ->
+	case esaml:check_dupe_ets(Assertion, Sha1) of
+		ok -> {ok, Req};
+		_ -> {duplicate, Req}
+	end.
+
+handle_consume(Assertion, Req) ->
 	Attrs = Assertion#esaml_assertion.attributes,
 	Uid = proplists:get_value(uid, Attrs),
 	Output = io_lib:format("<html><head><title>SAML SP demo</title></head><body><h1>Hi there!</h1><p>This is the <code>esaml_sp_default</code> demo SP callback module from eSAML.</p><table><tr><td>Your name:</td><td>\n~p\n</td></tr><tr><td>Your UID:</td><td>\n~p\n</td></tr></table><hr /><p>The assertion I got was:</p><pre>\n~p\n</pre></body></html>", [Assertion#esaml_assertion.subject#esaml_subject.name, Uid, Assertion]),
 	{ok, Req2} = cowboy_req:reply(200, [{<<"Content-Type">>, <<"text/html">>}], Output, Req),
-	{ok, Req2, State}.
+	{ok, none, Req2}.
 
-terminate(_Req, _State) ->
+terminate(_Req) ->
 	ok.

include/esaml.hrl
@@ -42,5 +42,5 @@
 	org = #esaml_org{} :: #esaml_org{}, tech = #esaml_contact{} :: #esaml_contact{},
 	key :: binary(), certificate :: binary(),
 	sp_sign_requests = false, idp_signs_assertions = true, idp_signs_envelopes = true,
-	sp_sign_metadata = false, trusted_fingerprints = [string() | binary()],
+	sp_sign_metadata = false, trusted_fingerprints :: [string() | binary()],
 	metadata_uri :: string(), consume_uri :: string()}).

src/esaml.app.src
@@ -8,7 +8,6 @@
   {applications, [
                   kernel,
                   stdlib,
-                  lager,
                   xmerl,
                   cowboy
                  ]},

src/esaml.erl
@@ -8,39 +8,30 @@
 
 -module(esaml).
 -behaviour(application).
+-behaviour(supervisor).
 
 -include_lib("xmerl/include/xmerl.hrl").
 -include_lib("public_key/include/public_key.hrl").
 -include("esaml.hrl").
 
--export([start/2, stop/1]).
--export([datetime_to_saml/1, saml_to_datetime/1]).
+-export([start/2, stop/1, init/1]).
+-export([stale_time/1]).
 -export([config/2, config/1, to_xml/1, decode_response/1, decode_assertion/1, validate_assertion/3]).
--export([build_nsinfo/2]).
 
 start(_StartType, _StartArgs) ->
-	Pid = spawn(fun() ->
-		register(esaml_ets_table_owner, self()),
-		ets:new(esaml_assertion_seen, [set, public, named_table]),
-		ets_table_owner()
-	end),
-	{ok, Pid}.
+	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 stop(_State) ->
 	ok.
 
-%% @doc Converts a calendar:datetime() into SAML time string
--spec datetime_to_saml(Time :: calendar:datetime()) -> string().
-datetime_to_saml(Time) ->
-	{{Y,Mo,D}, {H, Mi, S}} = Time,
-	lists:flatten(io_lib:format("~4.10.0B-~2.10.0B-~2.10.0BT~2.10.0B:~2.10.0B:~2.10.0BZ", [Y, Mo, D, H, Mi, S])).
-
--spec saml_to_datetime(Stamp :: binary() | string()) -> calendar:datetime().
-saml_to_datetime(Stamp) ->
-	StampBin = if is_list(Stamp) -> list_to_binary(Stamp); true -> Stamp end,
-	<<YBin:4/binary-unit:8, "-", MoBin:2/binary-unit:8, "-", DBin:2/binary-unit:8, "T", HBin:2/binary-unit:8, ":", MiBin:2/binary-unit:8, ":", SBin:2/binary-unit:8, "Z">> = StampBin,
-	F = fun(B) -> list_to_integer(binary_to_list(B)) end,
-	{{F(YBin), F(MoBin), F(DBin)}, {F(HBin), F(MiBin), F(SBin)}}.
+%% @internal
+init([]) ->
+	DupeEts = {esaml_ets_table_owner,
+		{esaml_util, start_ets, []},
+		permanent, 5000, worker, [esaml]},
+	{ok,
+		{{one_for_one, 60, 600},
+		[DupeEts]}}.
 
 %% @doc Retrieve a config record
 config(N) -> config(N, undefined).
@@ -48,31 +39,6 @@ config(N, D) ->
 	case application:get_env(esaml, N) of
 		{ok, V} -> V;
 		_ -> D
-	end.
-
--spec folduntil(F :: fun(), Acc :: term(), List :: []) -> AccOut :: term().
-folduntil(F, Acc, []) -> Acc;
-folduntil(F, Acc, [Next | Rest]) ->
-	case F(Next, Acc) of
-		{stop, AccOut} -> AccOut;
-		NextAcc -> folduntil(F, NextAcc, Rest)
-	end.
-
-thread([], Acc) -> Acc;
-thread([F | Rest], Acc) ->
-	thread(Rest, F(Acc)).
-
-threaduntil([], Acc) -> {ok, Acc};
-threaduntil([F | Rest], Acc) ->
-	case (catch F(Acc)) of
-		{'EXIT', Reason} ->
-			{error, Reason};
-		{error, Reason} ->
-			{error, Reason};
-		{stop, LastAcc} ->
-			{ok, LastAcc};
-		NextAcc ->
-			threaduntil(Rest, NextAcc)
 	end.
 
 response_map_status_code(R = #esaml_response{status = Code}) ->
@@ -145,7 +111,7 @@ common_attrib_map(Other) -> list_to_atom(Other).
 decode_response(Xml) ->
 	Ns = [{"samlp", 'urn:oasis:names:tc:SAML:2.0:protocol'},
 		  {"saml", 'urn:oasis:names:tc:SAML:2.0:assertion'}],
-	threaduntil([
+	esaml_util:threaduntil([
 		?xpath_attr_required("/samlp:Response/@Version", esaml_response, version, bad_version),
 		?xpath_attr_required("/samlp:Response/@IssueInstant", esaml_response, issue_instant, bad_response),
 		?xpath_attr("/samlp:Response/@Destination", esaml_response, destination),
@@ -158,7 +124,7 @@ decode_response(Xml) ->
 decode_assertion(Xml) ->
 	Ns = [{"samlp", 'urn:oasis:names:tc:SAML:2.0:protocol'},
 		  {"saml", 'urn:oasis:names:tc:SAML:2.0:assertion'}],
-	threaduntil([
+	esaml_util:threaduntil([
 		?xpath_attr_required("/saml:Assertion/@Version", esaml_assertion, version, bad_version),
 		?xpath_attr_required("/saml:Assertion/@IssueInstant", esaml_assertion, issue_instant, bad_assertion),
 		?xpath_attr_required("/saml:Assertion/saml:Subject/saml:SubjectConfirmation/saml:SubjectConfirmationData/@Recipient", esaml_assertion, recipient, bad_recipient),
@@ -170,7 +136,7 @@ decode_assertion(Xml) ->
 
 decode_assertion_subject(Xml) ->
 	Ns = [{"saml", 'urn:oasis:names:tc:SAML:2.0:assertion'}],
-	threaduntil([
+	esaml_util:threaduntil([
 		?xpath_text("/saml:Subject/saml:NameID/text()", esaml_subject, name),
 		?xpath_attr("/saml:Subject/saml:SubjectConfirmation/@Method", esaml_subject, confirmation_method),
 		?xpath_attr("/saml:Subject/saml:SubjectConfirmation/saml:SubjectConfirmationData/@NotOnOrAfter", esaml_subject, notonorafter),
@@ -179,7 +145,7 @@ decode_assertion_subject(Xml) ->
 
 decode_assertion_conditions(Xml) ->
 	Ns = [{"saml", 'urn:oasis:names:tc:SAML:2.0:assertion'}],
-	threaduntil([
+	esaml_util:threaduntil([
 		fun(C) ->
 			case xmerl_xpath:string("/saml:Conditions/@NotBefore", Xml, [{namespace, Ns}]) of
 				[#xmlAttribute{value = V}] -> [{not_before, V} | C]; _ -> C
@@ -218,19 +184,16 @@ decode_assertion_attributes(Xml) ->
 		end
 	end, [], Attrs)}.
 
-ets_table_owner() ->
-	receive
-		stop -> ok;
-		_ -> ets_table_owner()
-	end.
-
+%% @doc Returns the time at which an assertion is considered stale.
+-spec stale_time(#esaml_assertion{}) -> integer().
 stale_time(A) ->
-	thread([
+	esaml_util:thread([
 		fun(T) ->
 			case A#esaml_assertion.subject of
 				#esaml_subject{notonorafter = undefined} -> T;
 				#esaml_subject{notonorafter = Restrict} ->
-					Secs = calendar:datetime_to_gregorian_seconds(saml_to_datetime(Restrict)),
+					Secs = calendar:datetime_to_gregorian_seconds(
+						esaml_util:saml_to_datetime(Restrict)),
 					if (Secs < T) -> Secs; true -> T end;
 				_ -> T
 			end
@@ -240,14 +203,16 @@ stale_time(A) ->
 			case proplists:get_value(not_on_or_after, Conds) of
 				undefined -> T;
 				Restrict ->
-					Secs = calendar:datetime_to_gregorian_seconds(saml_to_datetime(Restrict)),
+					Secs = calendar:datetime_to_gregorian_seconds(
+						esaml_util:saml_to_datetime(Restrict)),
 					if (Secs < T) -> Secs; true -> T end
 			end
 		end,
 		fun(T) ->
 			if (T =:= none) ->
 				II = A#esaml_assertion.issue_instant,
-				IISecs = calendar:datetime_to_gregorian_seconds(saml_to_datetime(II)),
+				IISecs = calendar:datetime_to_gregorian_seconds(
+					esaml_util:saml_to_datetime(II)),
 				IISecs + 5*60;
 			true ->
 				T
@@ -265,53 +230,12 @@ check_stale(A) ->
 		A
 	end.
 
-check_dupe(A, Xml) ->
-	Now = erlang:localtime_to_universaltime(erlang:localtime()),
-	NowSecs = calendar:datetime_to_gregorian_seconds(Now),
-	DeathSecs = stale_time(A),
-	Digest = crypto:sha(
-		unicode:characters_to_binary(xmerl_c14n:c14n(xmerl_dsig:strip(Xml)))),
-	{ResL, _BadNodes} = rpc:multicall(erlang, apply, [fun() ->
-		case (catch ets:lookup(esaml_assertion_seen, Digest)) of
-			[{Digest, seen} | _] -> seen;
-			_ -> ok
-		end
-	end, []]),
-	case lists:member(seen, ResL) of
-		true ->
-			{error, duplicate_assertion};
-		_ ->
-			Until = DeathSecs - NowSecs + 1,
-			rpc:multicall(erlang, apply, [fun() ->
-				case ets:info(esaml_assertion_seen) of
-					undefined ->
-						Me = self(),
-						Pid = spawn(fun() ->
-							register(esaml_ets_table_owner, self()),
-							ets:new(esaml_assertion_seen, [set, public, named_table]),
-							ets:insert(esaml_assertion_seen, {Digest, seen}),
-							Me ! {self(), ping},
-							ets_table_owner()
-						end),
-						receive
-							{Pid, ping} -> ok
-						end;
-					_ ->
-						ets:insert(esaml_assertion_seen, {Digest, seen})
-				end,
-				{ok, _} = timer:apply_after(Until * 1000, erlang, apply, [fun() ->
-					ets:delete(esaml_assertion_seen, Digest)
-				end, []])
-			end, []]),
-			ok
-	end.
-
 validate_assertion(AssertionXml, Recipient, Audience) ->
 	case decode_assertion(AssertionXml) of
 		{error, Reason} ->
 			{error, Reason};
 		{ok, Assertion} ->
-			threaduntil([
+			esaml_util:threaduntil([
 				fun(A) -> case A of
 					#esaml_assertion{version = "2.0"} -> A;
 					_ -> {error, bad_version}
@@ -329,33 +253,16 @@ validate_assertion(AssertionXml, Recipient, Audience) ->
 						end;
 					_ -> A
 				end end,
-				fun check_stale/1,
-				fun(A) -> case check_dupe(A, AssertionXml) of ok -> A; _ -> {error, duplicate_assertion} end end
+				fun check_stale/1
 			], Assertion)
 	end.
-
-%% @internal
--spec build_nsinfo(#xmlNamespace{}, #xmlElement{}) -> #xmlElement{}.
-build_nsinfo(Ns, Attr = #xmlAttribute{name = Name}) ->
-	case string:tokens(atom_to_list(Name), ":") of
-		[NsPrefix, Rest] -> Attr#xmlAttribute{namespace = Ns, nsinfo = {NsPrefix, Rest}};
-		_ -> Attr#xmlAttribute{namespace = Ns}
-	end;
-build_nsinfo(Ns, Elem = #xmlElement{name = Name, content = Kids, attributes = Attrs}) ->
-	Elem2 = case string:tokens(atom_to_list(Name), ":") of
-		[NsPrefix, Rest] -> Elem#xmlElement{namespace = Ns, nsinfo = {NsPrefix, Rest}};
-		_ -> Elem#xmlElement{namespace = Ns}
-	end,
-	Elem2#xmlElement{attributes = [build_nsinfo(Ns, Attr) || Attr <- Attrs],
-					content = [build_nsinfo(Ns, Kid) || Kid <- Kids]};
-build_nsinfo(_Ns, Other) -> Other.
 
 %% @doc Convert a SAML request/metadata record into XML
 to_xml(#esaml_authnreq{issue_instant = Time, destination = Dest, issuer = Issuer, consumer_location = Consumer}) ->
 	Ns = #xmlNamespace{nodes = [{"samlp", 'urn:oasis:names:tc:SAML:2.0:protocol'},
 		  						{"saml", 'urn:oasis:names:tc:SAML:2.0:assertion'}]},
 
-	build_nsinfo(Ns, #xmlElement{name = 'samlp:AuthnRequest',
+	esaml_util:build_nsinfo(Ns, #xmlElement{name = 'samlp:AuthnRequest',
 		attributes = [#xmlAttribute{name = 'xmlns:samlp', value = proplists:get_value("samlp", Ns#xmlNamespace.nodes)},
 					  #xmlAttribute{name = 'xmlns:saml', value = proplists:get_value("saml", Ns#xmlNamespace.nodes)},
 					  #xmlAttribute{name = 'IssueInstant', value = Time},
@@ -421,7 +328,7 @@ to_xml(#esaml_sp_metadata{org = #esaml_org{name = OrgName, displayname = OrgDisp
 		]
 	},
 
-	build_nsinfo(Ns, #xmlElement{
+	esaml_util:build_nsinfo(Ns, #xmlElement{
 		name = 'md:EntityDescriptor',
 		attributes = [
 			#xmlAttribute{name = 'xmlns:md', value = atom_to_list(proplists:get_value("md", Ns#xmlNamespace.nodes))},
@@ -438,28 +345,8 @@ to_xml(#esaml_sp_metadata{org = #esaml_org{name = OrgName, displayname = OrgDisp
 to_xml(_) -> error("unknown record").
 
 
-
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
-
-build_nsinfo_test() ->
-	EmptyNs = #xmlNamespace{},
-	FooNs = #xmlNamespace{nodes = [{"foo", 'urn:foo:'}]},
-
-	E1 = #xmlElement{name = 'foo', content = [#xmlText{value = 'bar'}]},
-	E1 = build_nsinfo(EmptyNs, E1),
-
-	E2 = #xmlElement{name = 'foo:Blah', content = [#xmlText{value = 'bar'}]},
-	E2Ns = E2#xmlElement{nsinfo = {"foo", "Blah"}, namespace = FooNs},
-	E2Ns = build_nsinfo(FooNs, E2),
-
-	E3 = #xmlElement{name = 'blah:George', content = [E2]},
-	E3Ns = E3#xmlElement{nsinfo = {"blah", "George"}, namespace = FooNs, content = [E2Ns]},
-	E3Ns = build_nsinfo(FooNs, E3).
-
-datetime_test() ->
-	"2013-05-02T17:26:53Z" = datetime_to_saml({{2013,5,2},{17,26,53}}),
-	{{1990,11,23},{18,1,1}} = saml_to_datetime("1990-11-23T18:01:01Z").
 
 decode_response_test() ->
 	{Doc, _} = xmerl_scan:string("<samlp:Response xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\" xmlns:saml=\"urn:oasis:names:tc:SAML:2.0:assertion\" Version=\"2.0\" IssueInstant=\"2013-01-01T01:01:01Z\" Destination=\"foo\"></samlp:Response>", [{namespace_conformant, true}]),
@@ -516,11 +403,11 @@ decode_attributes_test() ->
 validate_assertion_test() ->
 	Now = erlang:localtime_to_universaltime(erlang:localtime()),
 	DeathSecs = calendar:datetime_to_gregorian_seconds(Now) + 1,
-	Death = esaml:datetime_to_saml(calendar:gregorian_seconds_to_datetime(DeathSecs)),
+	Death = esaml_util:datetime_to_saml(calendar:gregorian_seconds_to_datetime(DeathSecs)),
 
 	Ns = #xmlNamespace{nodes = [{"saml", 'urn:oasis:names:tc:SAML:2.0:assertion'}]},
 
-	E1 = build_nsinfo(Ns, #xmlElement{name = 'saml:Assertion',
+	E1 = esaml_util:build_nsinfo(Ns, #xmlElement{name = 'saml:Assertion',
 		attributes = [#xmlAttribute{name = 'xmlns:saml', value = "urn:oasis:names:tc:SAML:2.0:assertion"}, #xmlAttribute{name = 'Version', value = "2.0"}, #xmlAttribute{name = 'IssueInstant', value = "now"}],
 		content = [
 			#xmlElement{name = 'saml:Subject', content = [
@@ -538,7 +425,7 @@ validate_assertion_test() ->
 	{error, bad_recipient} = validate_assertion(E1, "foo", "something"),
 	{error, bad_audience} = validate_assertion(E1, "foobar", "something"),
 
-	E2 = build_nsinfo(Ns, #xmlElement{name = 'saml:Assertion',
+	E2 = esaml_util:build_nsinfo(Ns, #xmlElement{name = 'saml:Assertion',
 		attributes = [#xmlAttribute{name = 'xmlns:saml', value = "urn:oasis:names:tc:SAML:2.0:assertion"}, #xmlAttribute{name = 'Version', value = "2.0"}, #xmlAttribute{name = 'IssueInstant', value = "now"}],
 		content = [
 			#xmlElement{name = 'saml:Subject', content = [
@@ -549,47 +436,10 @@ validate_assertion_test() ->
 	}),
 	{error, bad_recipient} = validate_assertion(E2, "", "").
 
-validate_duplicate_assertion_test() ->
-	Now = erlang:localtime_to_universaltime(erlang:localtime()),
-	DeathSecs = calendar:datetime_to_gregorian_seconds(Now) + 1,
-	Death = esaml:datetime_to_saml(calendar:gregorian_seconds_to_datetime(DeathSecs)),
-
-	Ns = #xmlNamespace{nodes = [{"saml", 'urn:oasis:names:tc:SAML:2.0:assertion'}]},
-
-	E1 = build_nsinfo(Ns, #xmlElement{name = 'saml:Assertion',
-		attributes = [#xmlAttribute{name = 'xmlns:saml', value = "urn:oasis:names:tc:SAML:2.0:assertion"}, #xmlAttribute{name = 'Version', value = "2.0"}, #xmlAttribute{name = 'IssueInstant', value = "now"}],
-		content = [
-			#xmlElement{name = 'saml:Subject', content = [
-				#xmlElement{name = 'saml:SubjectConfirmation', content = [
-					#xmlElement{name = 'saml:SubjectConfirmationData',
-						attributes = [#xmlAttribute{name = 'Recipient', value = "foobar"},
-									  #xmlAttribute{name = 'NotOnOrAfter', value = Death}]
-					} ]} ]},
-			#xmlElement{name = 'saml:Conditions', content = [
-				#xmlElement{name = 'saml:AudienceRestriction', content = [
-					#xmlElement{name = 'saml:Audience', content = [#xmlText{value = "testAudience"}]}] }] } ]
-	}),
-	Digest = crypto:sha(
-		unicode:characters_to_binary(xmerl_c14n:c14n(xmerl_dsig:strip(E1)))),
-
-	[] = ets:lookup(esaml_assertion_seen, Digest),
-	{ok, _} = validate_assertion(E1, "foobar", "testAudience"),
-	[_] = ets:lookup(esaml_assertion_seen, Digest),
-	receive
-	after 500 ->
-		[_] = ets:lookup(esaml_assertion_seen, Digest),
-		{error, duplicate_assertion} = validate_assertion(E1, "foobar", "testAudience"),
-		receive
-		after 2000 ->
-			[] = ets:lookup(esaml_assertion_seen, Digest),
-			{error, stale_assertion} = validate_assertion(E1, "foobar", "testAudience")
-		end
-	end.
-
 validate_stale_assertion_test() ->
 	Ns = #xmlNamespace{nodes = [{"saml", 'urn:oasis:names:tc:SAML:2.0:assertion'}]},
-	OldStamp = esaml:datetime_to_saml({{1990,1,1}, {1,1,1}}),
-	E1 = build_nsinfo(Ns, #xmlElement{name = 'saml:Assertion',
+	OldStamp = esaml_util:datetime_to_saml({{1990,1,1}, {1,1,1}}),
+	E1 = esaml_util:build_nsinfo(Ns, #xmlElement{name = 'saml:Assertion',
 		attributes = [#xmlAttribute{name = 'xmlns:saml', value = "urn:oasis:names:tc:SAML:2.0:assertion"}, #xmlAttribute{name = 'Version', value = "2.0"}, #xmlAttribute{name = 'IssueInstant', value = "now"}],
 		content = [
 			#xmlElement{name = 'saml:Subject', content = [

src/esaml_cowboy_handler.erl
@@ -17,56 +17,14 @@
 
 -record(state, {idp_target, sp, max_saml_response_size}).
 
-ets_table_owner() ->
-	receive
-		stop -> ok;
-		_ -> ets_table_owner()
-	end.
-
 init(_Transport, Req, Options) ->
-	case {ets:info(esaml_privkey_cache), ets:info(esaml_certbin_cache)} of
-		{undefined, undefined} ->
-			Me = self(),
-			Pid = spawn(fun() ->
-				register(esaml_cowboy_ets_table_owner, self()),
-				ets:new(esaml_privkey_cache, [set, public, named_table]),
-				ets:new(esaml_certbin_cache, [set, public, named_table]),
-				Me ! {self(), ping},
-				ets_table_owner()
-			end),
-			receive
-				{Pid, ping} -> ok
-			end;
-		_ -> ok
-	end,
 	PrivKey = case proplists:get_value(sp_private_key, Options, esaml:config(sp_private_key)) of
 		undefined -> none;
-		PrivKeyPath ->
-			case ets:lookup(esaml_privkey_cache, PrivKeyPath) of
-				[{_, Key}] -> Key;
-				_ ->
-					{ok, KeyFile} = file:read_file(PrivKeyPath),
-					[KeyEntry] = public_key:pem_decode(KeyFile),
-					Key = case public_key:pem_entry_decode(KeyEntry) of
-						#'PrivateKeyInfo'{privateKey = KeyData} ->
-							public_key:der_decode('RSAPrivateKey', list_to_binary(KeyData));
-						Other -> Other
-					end,
-					ets:insert(esaml_privkey_cache, {PrivKeyPath, Key}),
-					Key
-			end
+		PrivKeyPath -> esaml_util:load_private_key(PrivKeyPath)
 	end,
 	Cert = case proplists:get_value(sp_certificate, Options, esaml:config(sp_certificate)) of
 		undefined -> none;
-		CertPath ->
-			case ets:lookup(esaml_certbin_cache, CertPath) of
-				[{_, CertBin}] -> CertBin;
-				_ ->
-					{ok, CertFile} = file:read_file(CertPath),
-					[{'Certificate', CertBin, not_encrypted}] = public_key:pem_decode(CertFile),
-					ets:insert(esaml_certbin_cache, {CertPath, CertBin}),
-					CertBin
-			end
+		CertPath -> esaml_util:load_certificate(CertPath)
 	end,
 	Tech = proplists:get_value(tech_contact, Options, esaml:config(tech_contact, [{name, "undefined"}, {email, "undefined"}])),
 

src/esaml_sp.erl
@@ -9,28 +9,17 @@
 -module(esaml_sp).
 
 -include("esaml.hrl").
+-include_lib("xmerl/include/xmerl.hrl").
 
--export([behaviour_info/1]).
--export([setup/1, authn_request/2, metadata/1, consume/3]).
+-export([setup/1, authn_request/2, metadata/1, consume/2, consume/3]).
 
-behaviour_info(callbacks) ->
-	[{init, 2}, {handle_assertion, 3}, {terminate, 2}];
-behaviour_info(_) ->
-	undefined.
+-callback init(Args :: term(), ModArgs :: term()) -> {ok, ModState :: term()}.
+-callback check_duplicate(Assertion :: #esaml_assertion{}, Sha1 :: binary(), ModState :: term()) -> {ok, ModState1 :: term()} | {duplicate, ModState1 :: term()}.
+-callback handle_consume(Assertion :: #esaml_assertion{}, ModState :: term()) -> {ok, ReturnValue :: term(), ModState1 :: term()}.
+-callback terminate(ModState :: term()) -> ok.
 
-threaduntil([], Acc) -> {ok, Acc};
-threaduntil([F | Rest], Acc) ->
-	case (catch F(Acc)) of
-		{'EXIT', Reason} ->
-			{error, Reason};
-		{error, Reason} ->
-			{error, Reason};
-		{stop, LastAcc} ->
-			{ok, LastAcc};
-		NextAcc ->
-			threaduntil(Rest, NextAcc)
-	end.
-
+%% @doc Initialize and validate an esaml_sp record
+-spec setup(#esaml_sp{}) -> #esaml_sp{}.
 setup(SP = #esaml_sp{trusted_fingerprints = FPs, metadata_uri = MetaURI,
 					 consume_uri = ConsumeURI}) ->
 	FPSources = FPs ++ esaml:config(trusted_fingerprints, []),
@@ -56,10 +45,17 @@ setup(SP = #esaml_sp{trusted_fingerprints = FPs, metadata_uri = MetaURI,
 		SP#esaml_sp{trusted_fingerprints = Fingerprints}
 	end.
 
+%% @doc Consume an assertion envelope in parsed XML
+-spec consume(Xml :: #xmlElement{} | #xmlDocument{}, #esaml_sp{}) -> {ok, ReturnValue :: term()} | {error, Reason :: term()}.
+consume(Xml, SP = #esaml_sp{}) ->
+	consume(Xml, [], SP).
+
+%% @doc Consume an assertion envelope in parsed XML, with additional arguments
+-spec consume(Xml :: #xmlElement{} | #xmlDocument{}, Args :: term(), #esaml_sp{}) -> {ok, ReturnValue :: term()} | {error, Reason :: term()}.
 consume(Xml, Args, SP = #esaml_sp{}) ->
 	Ns = [{"samlp", 'urn:oasis:names:tc:SAML:2.0:protocol'},
 		  {"saml", 'urn:oasis:names:tc:SAML:2.0:assertion'}],
-	threaduntil([
+	esaml_util:threaduntil([
 		fun(X) ->
 			case xmerl_xpath:string("/samlp:Response/saml:Assertion", X, [{namespace, Ns}]) of
 				[A] -> A;
@@ -70,7 +66,7 @@ consume(Xml, Args, SP = #esaml_sp{}) ->
 			if SP#esaml_sp.idp_signs_envelopes ->
 				case xmerl_dsig:verify(Xml, SP#esaml_sp.trusted_fingerprints) of
 					ok -> A;
-					OuterError -> {error, {outer_sig, OuterError}}
+					OuterError -> {error, {envelope, OuterError}}
 				end;
 			true -> A
 			end
@@ -79,7 +75,7 @@ consume(Xml, Args, SP = #esaml_sp{}) ->
 			if SP#esaml_sp.idp_signs_assertions ->
 				case xmerl_dsig:verify(A, SP#esaml_sp.trusted_fingerprints) of
 					ok -> A;
-					OuterError -> {error, {inner_sig, OuterError}}
+					InnerError -> {error, {assertion, InnerError}}
 				end;
 			true -> A
 			end
@@ -91,16 +87,24 @@ consume(Xml, Args, SP = #esaml_sp{}) ->
 			end
 		end,
 		fun(AR) ->
-			{ok, Req2, ModState} = apply(SP#esaml_sp.module, init, [Req, SP#esaml_sp.modargs]),
-			{ok, Req3, ModState2} = apply(SP#esaml_sp.module, handle_assertion, [Req2, AR, ModState]),
-			ok = apply(SP#esaml_sp.module, terminate, [Req3, ModState2]),
-			{Req3, ModState2}
+			{ok, ModState} = apply(SP#esaml_sp.module, init, [Args, SP#esaml_sp.modargs]),
+			case apply(SP#esaml_sp.module, check_duplicate, [AR, xmerl_dsig:digest(Xml), ModState]) of
+				{ok, ModState2} ->
+					{ok, ReturnValue, ModState3} = apply(SP#esaml_sp.module, handle_assertion, [AR, ModState2]),
+					ok = apply(SP#esaml_sp.module, terminate, [ModState3]),
+					ReturnValue;
+				{duplicate, ModState2} ->
+					ok = apply(SP#esaml_sp.module, terminate, [ModState2]),
+					{error, duplicate}
+			end
 		end
 	], Xml).
 
+%% @doc Return an AuthnRequest as an XML element
+-spec authn_request(IdpURL :: string(), #esaml_sp{}) -> #xmlElement{}.
 authn_request(IdpURL, SP = #esaml_sp{metadata_uri = MetaURI, consume_uri = ConsumeURI}) ->
 	Now = erlang:localtime_to_universaltime(erlang:localtime()),
-	Stamp = esaml:datetime_to_saml(Now),
+	Stamp = esaml_util:datetime_to_saml(Now),
 
 	Xml = esaml:to_xml(#esaml_authnreq{issue_instant = Stamp,
 									   destination = IdpURL,
@@ -112,6 +116,8 @@ authn_request(IdpURL, SP = #esaml_sp{metadata_uri = MetaURI, consume_uri = Consu
 		Xml
 	end.
 
+%% @doc Return the SP metadata as an XML element
+-spec metadata(#esaml_sp{}) -> #xmlElement{}.
 metadata(SP = #esaml_sp{org = Org, tech = Tech}) ->
 	Xml = esaml:to_xml(#esaml_sp_metadata{
 		org = Org,

commit e0c2dfb80581df5190a1ad7dbaf0f9cf094f58dc

fix crash introduced in c7177f8d520d2cb2c9031fec544449cd6cd8cee6

src/esaml_sp_default.erl
@@ -16,7 +16,7 @@
 init(Req, _Args) -> {ok, Req}.
 
 check_duplicate(Assertion, Sha1, Req) ->
-    case esaml:check_dupe_ets(Assertion, Sha1) of
+    case esaml_util:check_dupe_ets(Assertion, Sha1) of
         ok -> {ok, Req};
         _ -> {duplicate, Req}
     end.